17200adb1bc8f2d60ae1ad85e36c22851ead4704
dbakaev@mozilla.com <Dmitry Bakaev>
2019-03-05 15:45:32 +0200

    Replaced DEFAULT_UI_DELAY_LONG, added assert for find(), separated clicks with empty lines, added open_library() for library opening

--- iris/tests/bookmark/copy_bookmark_from_toolbar.py
+++ iris/tests/bookmark/copy_bookmark_from_toolbar.py
@@ -24,9 +24,13 @@ class Test(BaseTest):
         copy_option_pattern = Pattern('copy_option.png')
         iris_tab_pattern = Pattern('iris_tab.png')
 
+        iris_tab_available = exists(iris_tab_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        assert_true(self, iris_tab_available, '\'Iris\' tab is available after launching Firefox')
+
         area_to_click = find(iris_tab_pattern)
         area_to_click.x += 300
         area_to_click.y += 5
+
         right_click(area_to_click)
 
         bookmarks_toolbar_menu_option_available = exists(bookmarks_toolbar_menu_option_pattern)
@@ -34,32 +38,33 @@ class Test(BaseTest):
                     '\'Bookmarks Toolbar\' option is available in context menu')
 
         click(bookmarks_toolbar_menu_option_pattern)
-        bookmark_available_in_toolbar = exists(getting_started_bookmark_pattern, DEFAULT_UI_DELAY_LONG)
+
+        bookmark_available_in_toolbar = exists(getting_started_bookmark_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, bookmark_available_in_toolbar, 'The \'Bookmarks Toolbar\' is enabled.')
 
         right_click(getting_started_bookmark_pattern)
-        copy_option_available = exists(copy_option_pattern, DEFAULT_UI_DELAY_LONG)
+
+        copy_option_available = exists(copy_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, copy_option_available,
                     '\'Copy\' option is available in context menu after right-click at the bookmark')
 
         click(copy_option_pattern)
-        if Settings.is_mac():
-            type(text='b', modifier=KeyModifier.CMD + KeyModifier.SHIFT)
-        elif Settings.is_windows():
-            type(text='b', modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
-        elif Settings.is_linux():
-            type(text='o', modifier=KeyModifier.CTRL + KeyModifier.SHIFT)
 
-        other_bookmarks_folder_available = exists(other_bookmarks_folder_pattern, DEFAULT_FIREFOX_TIMEOUT)
+        open_library()
+        maximize_window()
+
+        other_bookmarks_folder_available = exists(other_bookmarks_folder_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, other_bookmarks_folder_available,
                     '\'Other bookmarks\' folder is available in Library window')
 
         right_click(other_bookmarks_folder_pattern)
-        paste_option_available = exists(paste_option_pattern, DEFAULT_UI_DELAY_LONG)
+
+        paste_option_available = exists(paste_option_pattern, DEFAULT_SHORT_FIREFOX_TIMEOUT)
         assert_true(self, paste_option_available,
                     '\'Paste\' option is available in context menu after right-click at the bookmarks folder')
 
         click(paste_option_pattern)
+
         bookmark_pasted_into_folder = exists(bookmark_in_other_bookmarks_folder_pattern, DEFAULT_UI_DELAY_LONG)
         assert_true(self, bookmark_pasted_into_folder,
                     'The bookmark from the previous step is pasted in the selected section.')
